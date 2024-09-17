f4465c482abf0d5e4f5cb843f2b9efa106c6ee85
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-09-17 14:25:31 -0300

    feat: aprendendo a fazer middleware para o idioma

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+        
+    }
+    
+    public async Task Invoke(HttpContext context)
+    {
+        
+    }
+}
