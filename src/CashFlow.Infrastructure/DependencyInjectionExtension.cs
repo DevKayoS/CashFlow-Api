da88149752bfa9dbeaee65cc9360bd6a15855320
Kayo <kayoribeirodev@gmail.com>
2024-09-21 14:50:27 -0300

    refactor: refatorando o repositorio para que seja possivel realizar a injecao de dependencia

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -1,5 +1,7 @@
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Infrastructure.DataAccess;
 using CashFlow.Infrastructure.DataAccess.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashFlow.Infrastructure;
@@ -8,6 +10,17 @@ public static class DependencyInjectionExtension
 {
     public static void AddInfraestructure(this IServiceCollection services)
     {
+        AddRepositories(services);
+        AddDbContext(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
         services.AddScoped<IExpensesRepository, ExpensesRepository>();
+    }
+    
+    private static void AddDbContext(IServiceCollection services)
+    {
+        services.AddDbContext<CashFlowDbContext>();
     }
 }
