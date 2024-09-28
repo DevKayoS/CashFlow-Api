8020fd7254d511a5bc036fc5fd6fda9525da29b9
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-09-28 12:26:11 -0300

    feat: formatando como vai ser exebido os dados no amount

--- src/CashFlow.Application/UseCases/Expenses/Report/GetExcel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/GetExcel/GenerateExpensesReportExcelUseCase.cs
@@ -7,6 +7,7 @@ namespace CashFlow.Application.UseCases.Report.GetExcel;
 
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
+    private const string CURRENCY_SYMBOL = "R$";
     private readonly IExpensesReadOnlyRepository _repository;
 
     public GenerateExpensesReportExcelUseCase(IExpensesReadOnlyRepository repository)
@@ -27,7 +28,7 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
 
         workbook.Author = "Kayo Vinicius";
         workbook.Style.Font.FontSize = 12;
-        workbook.Style.Font.FontName = "Times New Roman";
+        workbook.Style.Font.FontName = "Arial";
         
        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
         
@@ -40,11 +41,17 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
            worksheet.Cell($"A{raw}").Value = expense.Title;
            worksheet.Cell($"B{raw}").Value = expense.Date;
            worksheet.Cell($"C{raw}").Value = ConvertPaymentType(expense.PaymentType);
+           
            worksheet.Cell($"D{raw}").Value = expense.Amount;
+           worksheet.Cell($"D{raw}").Style.NumberFormat.Format = $"- {CURRENCY_SYMBOL} #,##0.00"; // ex: - R$12.00
+               
            worksheet.Cell($"E{raw}").Value = expense.Description;
 
            raw++;
        }
+        
+       // ajustando o preenchimento das colunas automaticamente
+       worksheet.Columns().AdjustToContents();
 
        var file = new MemoryStream();
        
