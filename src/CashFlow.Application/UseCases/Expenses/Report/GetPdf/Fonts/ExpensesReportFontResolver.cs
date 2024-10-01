9d7ddcde61a3dfc142a9ed3bfaa597676736dccd
Kayo <kayoribeirodev@gmail.com>
2024-09-30 23:40:32 -0300

    feat: terminando o resolvedor de font

--- src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/Fonts/ExpensesReportFontResolver.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/Fonts/ExpensesReportFontResolver.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Register.Report.GetPdf.Fonts;
@@ -6,10 +7,33 @@ public class ExpensesReportFontResolver : IFontResolver
 {
     public byte[]? GetFont(string faceName)
     {
+        var stream = ReadFontFile(faceName);
+
+        if (stream is null)
+        {
+            stream = ReadFontFile(FontHelper.DEFAULT_FONT);
+        }
+
+        var length = (int)stream!.Length;
+
+        var data = new byte[length];
+
+        stream.Read(buffer: data, offset: 0, count: length);
+
+        return data;
+
     }
 
     public FontResolverInfo? ResolveTypeface(string familyName, bool bold, bool italic)
     {
         return new FontResolverInfo(familyName);
+    }
+
+    // retorna os arquivos das fontes como stream
+    private Stream? ReadFontFile(string faceName)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return assembly.GetManifestResourceStream($"CashFlow.Application.UseCases.Expenses.Report.GetPdf.Fonts.{faceName}.ttfa");
     }
 }
--- src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/Fonts/FontHelper.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/Fonts/FontHelper.cs
@@ -2,8 +2,11 @@ namespace CashFlow.Application.UseCases.Expenses.Register.Report.GetPdf.Fonts;
 
 public class FontHelper
 {
+    // criando constantes com o nome dos arquivos
     public const string RALEWAY_REGULAR = "Raleway-Regular";
     public const string RALEWAY_BLACK = "Raleway-Black";
     public const string WORKSANS_REGULAR = "WorkSans-Regular";
     public const string WORKSANS_BLACK = "WorkSans-Black";
+
+    public const string DEFAULT_FONT = RALEWAY_REGULAR;
 }
