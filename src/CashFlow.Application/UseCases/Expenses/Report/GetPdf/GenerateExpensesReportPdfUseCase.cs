a0b114e7f67e761b2b060996b44a311ab9daafc3
Kayo <kayoribeirodev@gmail.com>
2024-10-01 23:45:50 -0300

    feat: estilizando documento pdf colocando o titulo, autor e setando como font padrao a Raleway regular no arquivo

--- src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/GenerateExpensesReportPdfUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Report/GetPdf/GenerateExpensesReportPdfUseCase.cs
@@ -1,5 +1,7 @@
 using CashFlow.Application.UseCases.Expenses.Register.Report.GetPdf.Fonts;
+using CashFlow.Domain.Reports;
 using CashFlow.Domain.Repositories.Expenses;
+using MigraDoc.DocumentObjectModel;
 using PdfSharp.Fonts;
 
 namespace CashFlow.Application.UseCases.Expenses.Register.Report.GetPdf;
@@ -22,7 +24,22 @@ public class GenerateExpensesReportPdfUseCase : IGenerateExpensesReportPdfUseCas
         {
             return [];
         }
+
+        var document = CreateDocument(month);
         
         return [];
+    }
+
+    private Document CreateDocument(DateOnly month)
+    {
+        var document = new Document();
+        document.Info.Title = $"Expenses for {month.ToString("Y")}";
+        document.Info.Author = "Kayo Vinicius";
+        
+        //font default is Raleway regular
+        var style = document.Styles["Normal"];
+        style!.Font.Name = FontHelper.RALEWAY_REGULAR;
+        
+        return document;
     }
 }
