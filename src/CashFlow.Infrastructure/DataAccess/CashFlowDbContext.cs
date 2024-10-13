232157e8d716336a18a1bd0187165f85ac4cbc53
Kayo <kayoribeirodev@gmail.com>
2024-10-13 15:51:24 -0300

    feat: criando a primeira migration

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -8,20 +8,3 @@ internal class CashFlowDbContext : DbContext
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
     public DbSet<Expense> Expenses { get; set; }
 }
-
-// comando config mysql shell> docker exec -it mysql1 mysql -uroot -p
-/*
- rodar essa query quando entrar no mysql
-
- *CREATE TABLE Expenses (
-          `Id` BIGINT NOT NULL AUTO_INCREMENT,
-          `Title` VARCHAR(255) NOT NULL,
-          `Description` VARCHAR(2000) NULL,
-          `Date` DATETIME NOT NULL,
-          `Amount` DECIMAL(10, 2) NOT NULL,
-          `PaymentType` INT NOT NULL,
-          PRIMARY KEY (`Id`)
-        );
-       *
- *
- */ 
