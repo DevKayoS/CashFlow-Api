08b4a2ce055a13cf97bf6c00954ea1acf98a6cc2
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-10-17 00:58:36 -0300

    feat: iniciando a verificacao para identificar se o email ja nao existe no banco de dados

---
+++ src/CashFlow.Infrastructure/DataAccess/User/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Domain.Repositories.User;
+
+namespace CashFlow.Infrastructure.DataAccess.User;
+
+public class UserRepository : IUserReadOnlyRepository
+{
+    private readonly CashFlowDbContext _dbContext;
+
+    public UserRepository(CashFlowDbContext dbContext) => _dbContext = dbContext;
+    
+    public Task<bool> ExistActiveUserWithEmail(string email)
+    {
+        var result = await _dbContext.
+    }
+}
