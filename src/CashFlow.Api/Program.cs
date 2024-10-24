486658b3afc6cafa268a4c199f71a3e83fa5cd3d
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-10-24 00:34:57 -0300

    feat: configurando a API para ela poder entender os tokens gerados

--- src/CashFlow.Api/Program.cs
+++ src/CashFlow.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Configuration;
+using System.Text;
 using CashFlow.Api.Filters;
 using CashFlow.Api.Middleware;
 using CashFlow.Application;
@@ -5,18 +7,37 @@ using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Infrastructure;
 using CashFlow.Infrastructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 // configurando injecao de dependencia do projeto de infraestrutura ja que esta como internal
 // pode ser feito assim -> DependencyInjectionExtension.AddInfraestructure(builder.Services);
 builder.Services.AddInfraestructure(builder.Configuration);
 
 builder.Services.AddApplication();
+
+//configurando a api para autenticacao
+var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey")
+    
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ClockSkew = new TimeSpan(0),
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+    };
+});
 
 // responsavel por fazer a api entender o filtro
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
@@ -34,6 +55,7 @@ app.UseMiddleware<CultureMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
