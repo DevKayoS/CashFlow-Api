b35a7bb498ece1e2803bf8de85cf45ba4a237724
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-10-23 23:53:26 -0300

    feat: finalizando o fluxo de login do usuario

--- src/CashFlow.Infrastructure/DataAccess/User/UserRepository.cs
+++ src/CashFlow.Infrastructure/DataAccess/User/UserRepository.cs
@@ -16,9 +16,9 @@ internal class UserRepository : IUserReadOnlyRepository, IUserWriteOnlyRepositor
        return await _dbContext.Users.AnyAsync(user => user.Email.Equals(email));
     }
 
-    public Task<Domain.Entities.User?> GetUserByEmail(string email)
+    public async Task<Domain.Entities.User?> GetUserByEmail(string email)
     {
-        throw new NotImplementedException();
+        return await _dbContext.Users.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
     }
 
     public async Task Add(Domain.Entities.User user)
