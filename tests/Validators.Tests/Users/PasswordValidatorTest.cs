7a5f329466890c3236293459bf827d51113ed27f
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-10-25 23:44:45 -0300

    tests: teste criado para validar  a senha que usuario

---
+++ tests/Validators.Tests/Users/PasswordValidatorTest.cs
@@ -0,0 +1,38 @@
+﻿using CashFlow.Application.UseCases.User;
+using CashFlow.Communication.Requests;
+using CashFlow.Exception;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+using FluentValidation;
+
+namespace Validators.Tests.Users.Register;
+
+public class PasswordValidatorTest
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("          ")]
+    [InlineData(null)]
+    [InlineData("a")]
+    [InlineData("aa")]
+    [InlineData("aaa")]
+    [InlineData("aaaa")]
+    [InlineData("aaaaa")]
+    [InlineData("aaaaaa")]
+    [InlineData("aaaaaaa")]
+    [InlineData("aaaaaaaa")]
+    [InlineData("Aaaaaaaa")]
+    [InlineData("AAAAAAAAA")]
+    [InlineData("Aaaaaaaa1")]
+    public void Error_Password_Invalid(string password)
+    {
+        // arrange
+        var validator = new PasswordValidator<RequestRegisterUserJson>();
+        
+        // act
+        var result = validator.IsValid(new ValidationContext<RequestRegisterUserJson>(new RequestRegisterUserJson()), password);
+        
+        // assert
+        result.Should().BeFalse();
+    }
+}
--- tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
+++ tests/Validators.Tests/Users/Register/RegisterUserValidatorTest.cs
@@ -1,13 +1,13 @@
 ﻿using CashFlow.Application.UseCases.User;
-using CashFlow.Communication.Requests;
 using CashFlow.Exception;
 using CommonTestUtilities.Requests;
 using FluentAssertions;
 
 namespace Validators.Tests.Users.Register;
 
-public class PasswordValidatorTest
+public class RegisterUserValidatorTest
 {
+    
     [Fact]
     public void Sucess()
     {
@@ -58,5 +58,37 @@ public class PasswordValidatorTest
         // assert
         result.IsValid.Should().BeFalse();
         result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_EMPTY));
+    }
+    
+    [Fact]
+    public void Error_Email_Invalid()
+    {
+        // arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestsRegisterUserJsonBuilder.Build();
+        request.Email = "teste.com";
+        
+        // act
+        var result = validator.Validate(request);
+        
+        // assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.EMAIL_INVALID));
+    }
+    
+    [Fact]
+    public void Error_Password_Empty()
+    {
+        // arrange
+        var validator = new RegisterUserValidator();
+        var request = RequestsRegisterUserJsonBuilder.Build();
+        request.Password = string.Empty;
+        
+        // act
+        var result = validator.Validate(request);
+        
+        // assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ResourceErrorMessages.INVALID_PASSWORD));
     }
 }
