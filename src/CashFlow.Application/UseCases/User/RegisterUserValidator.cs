40680c3f2cb4d5f4e8746398d9f31f690f0f67fa
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-10-25 23:26:51 -0300

    test: ajustando teste para verificar se o erro de email vazio e disparado

--- src/CashFlow.Application/UseCases/User/RegisterUserValidator.cs
+++ src/CashFlow.Application/UseCases/User/RegisterUserValidator.cs
@@ -10,8 +10,11 @@ public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
     public RegisterUserValidator()
     {
         RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
-        RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_EMPTY).EmailAddress()
-            .WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+        RuleFor(user => user.Email)
+            .NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_EMPTY)
+            .EmailAddress()
+            .When(user => string.IsNullOrWhiteSpace(user.Email )== false, ApplyConditionTo.CurrentValidator) //sera executada apenas se o email vier preenchido 
+            .WithMessage(ResourceErrorMessages.EMAIL_INVALID); 
         RuleFor(user => user.Password).SetValidator(new PasswordValidator<RequestRegisterUserJson>());
     }
 }
